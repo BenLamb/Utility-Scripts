d058cb6213a1808714315b5fa26250c48f6a1713
Ben Lamb <ben@notedigital.co.uk>
2015-03-03 12:56:45 +0000

    Added position: fixed and allowed Javascript to execute in output

--- php-executer.php
+++ php-executer.php
@@ -5,15 +5,45 @@
  * PHP without having to save any files, but don't try to use it for anything
  * other than that!
  */
+
+// allow Javascript to be included in the POSTed code - Chrome stops its execution 
+// without this header due to risk of XSS - and rightly so!
+header('X-XSS-Protection: 0');
 ?>
 <html>
 <head>
 	<title>PHP Executer</title>
+	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$code_textarea = $('[name=code]');
+			$code_textarea.keydown(function(e) {
+				var keyCode = e.keyCode || e.which; 
+				
+				// check for 'tab'
+				if (keyCode == 9) {
+					e.preventDefault();
+					
+					var caretPos = $code_textarea[0].selectionStart;
+					var textAreaTxt = $code_textarea.val();
+					var txtToAdd = "	";
+					$code_textarea.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+					$code_textarea[0].setSelectionRange(caretPos + 1, caretPos + 1);
+				}
+				
+				// check for F5 - submit form
+				if (keyCode == 116) {
+					e.preventDefault();
+					$('form').submit();
+				}
+			});
+		});
+	</script>
 </head>
 
 <body>
-<form method="post" style="margin: 0px; float: left; width: <?php echo (!empty($_POST) ? '49%' : '100%'); ?>; height: 100%">
-	<textarea name="code" style="width: 100%; height: 90%"><?php echo (isset($_POST['code']) ? $_POST['code'] : ''); ?></textarea>
+<form method="post" style="margin: 0px; float: left; width: <?php echo (!empty($_POST) ? '49%' : '100%'); ?>; height: 100%; position: fixed">
+	<textarea name="code" style="width: 100%; height: 90%"><?php echo (isset($_POST['code']) ? stripslashes($_POST['code']) : ''); ?></textarea>
 	<br /><input type="submit" />
 </form>
 
